09f2f45b69f43c12f8809ca46249f24adb42b7c9
sathyamoorthysc <sathyamoorthy232@gmail.com>
2020-03-17 05:54:17 +0530

    MVC Recipe Project - Starter

---
+++ MVCRecipe/src/main/java/guru/springframework/mvcrecipe/MvcRecipeApplication.java
@@ -0,0 +1,13 @@
+package guru.springframework.mvcrecipe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MvcRecipeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MvcRecipeApplication.class, args);
+	}
+
+}
---
+++ MVCRecipe/src/test/java/guru/springframework/mvcrecipe/MvcRecipeApplicationTests.java
@@ -0,0 +1,13 @@
+package guru.springframework.mvcrecipe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MvcRecipeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
